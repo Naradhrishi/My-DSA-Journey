49ac61b2bc5f7b6264739e968b14a8942080f31f
Naradhrishi <kumarnarad2111@gmail.com>
2026-05-23 22:37:57 +0530

    Solved binary search easy problem Guess Number

---
+++ LeetCode/BinarySearch/guessNumber.java
@@ -0,0 +1,28 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int lb = 1, ub = n;
+        while(lb <= ub){
+            int mid = lb + (ub - lb) / 2;
+            int res = guess(mid);
+            if(res == 0){
+                return mid;
+            }
+            if(res == -1){
+                ub = mid - 1;
+            }else if(res == 1){
+                lb = mid + 1;
+            }
+        }
+        return lb;
+        
+    }
+}
