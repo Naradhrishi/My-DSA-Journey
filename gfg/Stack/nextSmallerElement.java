fcd9e874c46c8c8a7a94d0596e9236c428dab057
Naradhrishi <kumarnarad2111@gmail.com>
2026-08-23 19:36:18 +0530

    refactor: remove redundent code for this problem

--- gfg/Stack/nextSmallerElement.java
+++ gfg/Stack/nextSmallerElement.java
@@ -1,32 +1,22 @@
 class Solution {
     static ArrayList<Integer> nextSmallerEle(int[] arr) {
-        ArrayList<Integer> final_res = new ArrayList<>();
+        ArrayList<Integer> next_smaller = new ArrayList<>();
         int[] res = new int[arr.length];
         Deque<Integer> stack = new ArrayDeque<>();
         
-        
-        for(int i=arr.length-1; i >= 0; i--){
-            if(stack.isEmpty()){
-                res[i] = -1;
-            }else{
-                while(!stack.isEmpty()){
-                    int top = stack.peek();
-                    if(top < arr[i]){
-                        res[i] = top;
-                        break;
-                    }else{
-                        stack.pop();
-                    }
-                    if(stack.isEmpty()){
-                        res[i] = -1;
-                    }
-                }
+        for(int i = arr.length-1; i >= 0; i--){
+            while(!stack.isEmpty() && stack.peek() >= arr[i]){
+                stack.pop();
             }
+            
+            res[i] = stack.isEmpty() ? -1 : stack.peek();
             stack.push(arr[i]);
         }
+        
         for(int v : res){
-            final_res.add(v);
+            next_smaller.add(v);
         }
-        return final_res;
+        
+        return next_smaller;
     }
 }
