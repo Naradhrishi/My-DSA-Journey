f3a1ecf49e627cda124dfb331d215f94f5ca0b97
Naradhrishi <kumarnarad2111@gmail.com>
2026-08-29 23:22:55 +0530

    refactor: replace nested if else with greedy array lookup for int to roman

--- LeetCode/Math/integerToRoman.java
+++ LeetCode/Math/integerToRoman.java
@@ -1,73 +1,16 @@
 class Solution {
     public String intToRoman(int num) {
         StringBuilder roman = new StringBuilder();
+        int[] numbers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romanSymbol = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-        while(num != 0){
-            if(num > 999){
-                roman.append('M');
-                num -= 1000;
-            }else if(num >= 500){
-                // check if it starts with 9
-                int firstDigit = getFirstDigit(num);
-                if(firstDigit == 9){
-                    roman.append("CM");  
-                    num -= 900;
-                }else{
-                    roman.append('D');
-                    num -= 500;
-                }
-            }else if(num >= 100){
-                // check if it starts with 4
-                int firstDigit = getFirstDigit(num);
-                if(firstDigit == 4){
-                    roman.append("CD");
-                    num -= 400;
-                }else{
-                    roman.append('C');
-                    num -= 100;
-                }
-            }else if(num >= 50){
-                int firstDigit = getFirstDigit(num);
-                if(firstDigit == 9){
-                    roman.append("XC");
-                    num -= 90;
-                }else{
-                    roman.append('L');
-                    num -= 50;
-                }
-            }else if(num >= 10){
-                int firstDigit = getFirstDigit(num);
-                if(firstDigit == 4){
-                    roman.append("XL");
-                    num -= 40;
-                }else{
-                    roman.append('X');
-                    num -= 10;
-                }
-            }else if(num >= 5){
-                if(num == 9){
-                    roman.append("IX");
-                    num -= 9;
-                }else{
-                    roman.append('V');
-                    num -= 5;
-                }
-            }else{
-                if(num == 4){
-                    roman.append("IV");
-                    num -= 4;
-                }else{
-                    roman.append('I');
-                    num -= 1;
-                }
+        for(int i=0; i<numbers.length; i++){
+            while(num >= numbers[i]){
+                roman.append(romanSymbol[i]);
+                num -= numbers[i];
             }
         }
         return roman.toString();
-    }
-    public int getFirstDigit(int n){
-        while(n > 9){
-            n = n/10;
-        }
-        return n;
+    
     }
 }
